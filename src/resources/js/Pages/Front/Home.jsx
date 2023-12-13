096cde984972b2936cb2c622ac198fcb75395f45
森島瞬也 <245224aa@gmail.com>
2023-12-13 18:39:55 +0900

    first commit

--- src/resources/js/Pages/Front/Home.jsx
+++ src/resources/js/Pages/Front/Home.jsx
@@ -5,6 +5,9 @@ import { Head } from '@inertiajs/react';
 const Home = (props) => {
   const { brands, prefuctures, keywords } = props;
 
+  console.log(import.meta.env.VITE_APP_URL || 'false')
+  console.log(import.meta.env.APP_URL || 'false')
+
   return (
     <>
       <FrontLayout>
