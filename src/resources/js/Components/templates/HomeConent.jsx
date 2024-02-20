12fdadaa46a4eb4d3b9501e5f44de75124d6de62
森島瞬也 <245224aa@gmail.com>
2024-02-20 19:56:58 +0900

    change:form高さ修正

--- src/resources/js/Components/templates/HomeConent.jsx
+++ src/resources/js/Components/templates/HomeConent.jsx
@@ -4,7 +4,7 @@ const HomeContent = ({ keywords, brands, prefuctures }) => {
 
   return (
     <div
-      className="bg-[url(./images/background-top2.jpg)] sm:bg-[url(./images/background-top.jpg)] flex  h-[400px] w-full items-center  justify-center  bg-center bg-no-repeat sm:h-[437px]   "
+      className="sm:bg-[url(./images/background-top.jpg)] flex  h-[400px] w-full items-center  justify-center  bg-center bg-no-repeat sm:h-[437px] "
     >
       <div className="w-full px-1 sm:max-w-[800px] sm:px-0 ">
         <div className="flex justify-center ">
